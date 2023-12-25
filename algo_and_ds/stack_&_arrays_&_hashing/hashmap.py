0fde5427c82de85a1154320bdf065602445a432f
Md Raihan Ahmed <raihan.ahmed05.96@gmail.com>
2023-12-24 23:56:56 -0700

    dynamic arrays, stack, hashmap

---
+++ algo_and_ds/stack_&_arrays_&_hashing/hashmap.py
@@ -0,0 +1,59 @@
+'''
+A hashtable doesn't allow keys or values to set to null but a hashmap does allow it.
+In python a default dictionary is a hashtable - dict = {}
+Common ops in dict python,
+    1. Insert => hashtable["key"] = "value"
+    2. Remove => hashtable.pop("item")
+                 hashtable.popitem() - this will return the most last item
+    3. search => hashtable["key"]
+        Using loop:
+            for k in hashtable: => it will give only keys
+            for k in hashtable.keys() => give only keys
+            for value in hashtable.values() => give only values
+            for k,v in hashtable.items() => gives both keys and values
+
+But a defaultdict(list) is a hashmap. 
+    1. all same as hashtable  
+'''
+from collections import defaultdict
+
+# hashtable
+print("Hashtable:\n")
+thisdict =	{}
+thisdict["color"] = "red"
+thisdict["value1"] = "a"
+thisdict["value2"] = "b"
+print(thisdict)
+print(thisdict.pop("value2"))
+print(thisdict.popitem())
+thisdict["value3"] = "c"
+thisdict["value4"] = "d"
+for k,v in thisdict.items():
+    print(k,v)
+
+#hashmap - defaultdict
+hashmap = defaultdict(list)
+hashmap["v1"] = "a"
+hashmap["v2"] = "b"
+hashmap["v3"] = "c"
+print("hashmap:\n",hashmap)
+print(hashmap.pop("v2"))
+print(hashmap.popitem())
+for k,v in hashmap.items():
+    print(k,v)
+
+''' - problem - group anagrams
+from collections import defaultdict
+
+defaultdict
+
+ans = defaultdict(list)
+strs = ["eat","tea","tan","ate","nat","bat"]
+for s in strs:
+    count = [0] * 26
+    for c in s:
+        count[ord(c) - ord("a")] += 1
+    ans[tuple(count)].append(s)
+
+print(ans.values())
+'''
---
+++ algo_and_ds/stack_&_arrays_&_hashing/stack_or_dynamicArray.py
@@ -0,0 +1,36 @@
+'''
+Stack is LIFO - Last In First Out
+All the operations of stack can be performed with the defaul array python.
+
+3 major operations in stack,
+    1. push => stack.append()
+    2. pop => stack.pop()
+    3. peek/top => stack[0]
+
+We can also do following operations in dynamic array,
+    1. arr.index(element, start, end) => check if element is present from this starting index to the ending index.
+    2. arr.remove(element) => remove this element from the array
+    3. arr.reverse() => reverse the list
+    4. arr.sort() => sort the array
+'''
+
+darray = []
+darray.append(1)
+darray.append(2)
+darray.append(3)
+darray.append(4)
+darray.append(5)
+darray.append(6)
+
+print("darray:",darray)
+print("darray.pop(): ",darray.pop())
+print("darray.index(element,start,end) => darray.index(4,0,4), index of element 4 from index 0 to 4 is: ", darray.index(4,0,4))
+
+darray.remove(4)
+print("darray.remove(4), after removing 4, new array is: ",darray)
+
+darray.reverse()
+print("darray.reverse(), after reversing the array, new array is: ", darray)
+
+darray.sort()
+print("darray.sort(), after sorting the array: ", darray)
